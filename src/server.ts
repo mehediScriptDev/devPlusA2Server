79fb5922e5ed377415560d8c83dbc0c70cc40352
mehedi <mehedi25122003@gmail.com>
2026-06-04 22:58:33 +0600

    server added professionally

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import app from "./app"
+import app from "./app";
 import config from "./config";
+import { initializeDatabase } from "./config/db.js";
 
-const main = () =>{
-    app.listen(config.port,()=>{
+const main = async () => {
+    await initializeDatabase();
+    app.listen(config.port, () => {
         console.log(`see? this is my port number man: ${config.port}`);
-        
-    })
-}
+    });
+};
 main();
